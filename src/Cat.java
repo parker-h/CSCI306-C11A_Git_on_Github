d585db0495522e519071977ebd22173df4a5a922
HArnarsson <harnarsson@mines.edu>
2023-09-26 11:35:36 -0600

    changing height of eyes

--- src/Cat.java
+++ src/Cat.java
@@ -6,8 +6,8 @@ public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
-	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
+	// eyes will be about 1/5 from top of head and 1/4 from left
+	private static final int EYE_Y = HEAD_DIMENSION/5;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
